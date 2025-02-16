39a16939037e888fa873c9fe9bff5eb0c2db0d14
Timothy T. Joe <106974662+TimTJoe@users.noreply.github.com>
2025-02-16 23:11:55 +0000

    Add CRUD controllers

---
+++ controller/create.controller.js
@@ -0,0 +1,15 @@
+const { createRecord } = require("@lib/crud.lib");
+const {getTableName} = require('drizzle-orm')
+
+
+
+const create = (schema) => async (req, res) => {
+  try {
+    const newRecord = await createRecord(req.db, schema, req.body);
+    res.status(201).json({ message: `${getTableName(schema)}  created successfully.`, data: newRecord });
+  } catch (error) {
+    res.status(500).json({ message: `Failed to create ${getTableName(schema)}.`, error });
+  }
+};
+
+module.exports = { create };
